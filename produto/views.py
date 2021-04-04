db1091cd6bb8b36af563f314dcf4c7ddc1f358d4
apla77 <verocidade77@gmail.com>
2021-04-04 16:52:36 -0300

    Salvando o pedido do cliente e correções de bugs.

--- produto/views.py
+++ produto/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
-from django.http import HttpResponse
 from django.contrib import messages
 from . import models
+from perfil.models import Perfil
 
 
 class ListaProdutos(ListView):
@@ -149,6 +149,22 @@ class ResumoDaCompra(View):
     def get(self, *args, **kwargs):
         if not self.request.user.is_authenticated:
             return redirect('perfil:criar')
+
+        perfil = Perfil.objects.filter(usuario=self.request.user).exists()
+
+        if not perfil:
+            messages.error(
+                self.request,
+                'Usuário sem perfil.'
+            )
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio.'
+            )
+            return redirect('produto:lista')
 
         contexto = {
             'usuario': self.request.user,
